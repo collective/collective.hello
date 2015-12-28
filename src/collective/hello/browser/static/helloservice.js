739ef7004f8db4232c3faae9997cd1317ba31401
Simon PREVIDENTE <simon.previdente@free.fr>
2015-12-28 14:19:32 +0100

    Remove JQuery

--- src/collective/hello/browser/static/helloservice.js
+++ src/collective/hello/browser/static/helloservice.js
@@ -1,32 +1,40 @@
-(function ($) {
-  'use strict'
-  $(document).ready(function () {
-    $('#hCRForm').submit(function (e) {
-      // Intercept Form Submission
-      e.preventDefault()
+(function () {
+    "use strict";
+    document.addEventListener("DOMContentLoaded", function () {
+        var hCRForm = document.getElementById("hCRForm");
+        if((typeof hCRForm != "undefined") && (hCRForm != null)) {
+            hCRForm.addEventListener("submit", function (e) {
+                // Intercept Form Submission
+                e.preventDefault();
 
-      var helloSubmit = document.getElementById('hCRSubmit')
-      var dataBaseUrl = document.getElementsByTagName('Body')[0].getAttribute('data-base-url')
-      var authenticator = document.getElementById('hCRAuth').value
+                var helloSubmit = document.getElementById("hCRSubmit");
+                var dataBaseUrl = document.getElementsByTagName("Body")[0].getAttribute("data-base-url");
+                var authenticator = document.getElementById("hCRAuth").value;
 
-      // Specifies that the action is in progress
-      helloSubmit.value = 'Loading...'
-      helloSubmit.disabled = true
+                // Specifies that the action is in progress
+                helloSubmit.value = "Loading...";
+                helloSubmit.disabled = true;
 
-      $.ajax({
-        async: true,
-        type: 'POST',
-        url: dataBaseUrl + '/helloService',
-        data: { _authenticator: authenticator },
-        success: function (result) {
-          helloSubmit.value = 'Room available !'
-          window.open('https://hello.firefox.com/' + result.roomToken)
-          window.location.reload()
-        },
-        error: function (result) {
-          helloSubmit.value = 'Error'
+                fetch(dataBaseUrl+"/helloService", {
+                	method: "POST",
+                	headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                	},
+                  credentials: 'same-origin',
+                  body: JSON.stringify({ _authenticator: authenticator})
+                }).then(function (response) {
+                    helloSubmit.value = "Room available !";
+                    console.log(response);
+                    response.json().then(function(r) {
+                        window.open("https://hello.firefox.com/" + r.roomToken);
+                    });
+                }).catch(function (error) {
+                    helloSubmit.value = "Error";
+                });
+
+                return false;
+            })
         }
-      })
-    })
-  })
-})(jQuery)
+    });
+})();
